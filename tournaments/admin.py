d33f0dbc15f4e08c8f6f822d70e9347512ecee8c
idanmel <idanmel@gmail.com>
2023-12-19 14:57:47 +0200

    added team and match

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Tournament, Phase
+from .models import Tournament, Phase, Team, Match
 
 admin.site.register([
     Tournament,
     Phase,
+    Team,
+    Match,
 ])
--- tournaments/models.py
+++ tournaments/models.py
@@ -13,3 +13,25 @@ class Phase(models.Model):
 
     def __str__(self):
         return f"{self.tournament} - {self.name}"
+
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+    flag = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class Match(models.Model):
+    home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="home_team")
+    away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="away_team")
+    phase = models.ForeignKey(Phase, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    ordering = ['date']
+
+    def __str__(self):
+        return f"{self.phase}: {self.home_team} - {self.away_team}"
+
+    class Meta:
+        verbose_name_plural = "Matches"
+    
