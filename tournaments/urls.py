acbb5313c1e1f4d6841607e8bdd2efd2db8bd23c
idanmel <idanmel@gmail.com>
2023-12-25 14:28:18 +0200

    login endpoint

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # path("", views.index, name="index"),
     path('tournaments/', views.TournamentView.as_view(), name='tournaments'),
     path('tournaments/<int:tournament_id>/matches', views.MatchesView.as_view(), name='matches'),
+    path('tournaments/login', views.LoginView.as_view(), name='login'),
 ]
--- tournaments/views.py
+++ tournaments/views.py
@@ -1,9 +1,17 @@
 from django.views import View
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import TournamentSerializer, MatchSerializer
 from .models import Tournament, Match
+
+import json
+
+
+class HttpUnprocessableEntity(HttpResponse):
+    status_code = 422
 
 
 class TournamentView(APIView):
@@ -18,3 +26,15 @@ class MatchesView(APIView):
         matches = Match.objects.filter(phase__tournament_id=tournament_id)
         serializer = MatchSerializer(matches, many=True)
         return Response({"data": serializer.data})
+
+class LoginView(View):
+
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+    
+    def post(self, request):
+        j = json.loads(request.body)
+        if not j.get("token"):
+            return HttpUnprocessableEntity("Missing Token field")
+        return HttpResponse(j["token"])
